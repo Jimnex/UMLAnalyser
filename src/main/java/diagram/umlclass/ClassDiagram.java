78304ca035c092033e3dc3e1ddec757883c6f8f7
Izsóf <balazs.izsof@gmail.com>
2019-08-25 09:54:06 +0200

    ClassDiagram class collection init

--- src/main/java/diagram/umlclass/ClassDiagram.java
+++ src/main/java/diagram/umlclass/ClassDiagram.java
@@ -2,7 +2,19 @@ package diagram.umlclass;
 
 import diagram.Diagram;
 
+import java.util.Collection;
+
 public class ClassDiagram extends Diagram {
+    public Collection<Class> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(Collection<Class> classes) {
+        this.classes = classes;
+    }
+
+    private Collection<Class> classes;
+
     public ClassDiagram(String name) {
         super(name);
     }
