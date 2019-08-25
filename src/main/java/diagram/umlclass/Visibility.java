c3618f786d026b869f842bdf86197fdcc050ee16
Izsóf <balazs.izsof@gmail.com>
2019-08-25 08:40:15 +0200

    Visibility Enum createFromStr()

--- src/main/java/diagram/umlclass/Visibility.java
+++ src/main/java/diagram/umlclass/Visibility.java
@@ -3,5 +3,16 @@ package diagram.umlclass;
 public enum Visibility {
     PUBLIC,
     PRIVATE,
-    INTERNAL
+    INTERNAL;
+
+    public static Visibility createFromStr(String str){
+        if (str == "Public"){
+            return Visibility.PUBLIC;
+        } else if (str == "Private"){
+            return Visibility.PRIVATE;
+        } else {
+            return Visibility.INTERNAL;
+        }
+    }
+
 }
