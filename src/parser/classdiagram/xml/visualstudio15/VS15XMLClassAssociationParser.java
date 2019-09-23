f7afacfa9705cd9c95e05ab2d8d8b9261612bd5c
Izsóf Balázs <balazs.izsof@gmail.com>
2019-09-23 19:58:42 +0200

    Visual studio 15 implementation

--- src/parser/classdiagram/xml/visualstudio15/VS15XMLClassAssociationParser.java
+++ src/parser/classdiagram/xml/visualstudio15/VS15XMLClassAssociationParser.java
@@ -1,51 +1,75 @@
 package parser.classdiagram.xml.visualstudio15;
 
+import diagram.umlclass.Aggregation;
 import diagram.umlclass.Association;
+import diagram.umlclass.AssociationNode;
+import diagram.umlclass.Multiplicity;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import parser.Parser;
+import parser.XML;
+import parser.classdiagram.AssociationParser;
 
-public class VS15XMLClassAssociationParser implements Parser<Association>{
-    private Element element;
-    private Association association;
+public class VS15XMLClassAssociationParser extends AssociationParser {
+    private Node node;
+    private Node target;
+    private Node source;
 
-    public Association getAssociation() {
-        return association;
+    public VS15XMLClassAssociationParser(Node associationNode) {
+        this.node = associationNode;
+        this.source = XML.getNode(this.node,"relationshipOwnedElementsInternal/associationHasOwnedEnds[0]/memberEnd");
+        this.target = XML.getNode(this.node,"relationshipOwnedElementsInternal/associationHasOwnedEnds[1]/memberEnd");
     }
 
-    public VS15XMLClassAssociationParser(Element associationElement) {
-        this.element = associationElement;
-    }
-
-
-        //XmlNode sourceNode = aNode.SelectSingleNode("cd:relationshipOwnedElementsInternal",nsmgr).ChildNodes.Item(0).FirstChild;
-        //XmlNode targetNode = aNode.SelectSingleNode("cd:relationshipOwnedElementsInternal", nsmgr).ChildNodes.Item(1).FirstChild;
-
-        //a.Source = getCommonAssociationMemberAttributes(sourceNode);
-        //a.Source.ClassName = className;
-        //a.Source.ClassID = id;
-
-        //a.Target = getCommonAssociationMemberAttributes(targetNode);
-        //a.Target.ClassName = getAttribute(aNode.SelectSingleNode("cd:classMoniker",nsmgr),"LastKnownName");
-        //a.Target.ClassID = getAttribute(aNode.SelectSingleNode("cd:classMoniker",nsmgr),"Id");
 
     @Override
-    public Association parse() {
-        Node associationXMLNodes = this.element.getChildNodes().item(3);
-        //this.association = new Association(this.parseAssociationNodeElement((Element)associationXMLNodes.item(0)),this.parseAssociationNodeElement((Element)associationXMLNodes.item(1)));
-        return this.association;
+    protected AssociationNode parseAssociationNode(Boolean isSource) {
+        if(isSource){
+            return this.parseAssociationNode(source);
+        } else {
+            return this.parseAssociationNode(target);
+        }
     }
 
+    @Override
+    protected boolean parseIsComposite() {
+        return XML.getBooleanValue(this.source, "isComposite");
+    }
 
+    @Override
+    protected Aggregation parseAggregation() {
+        return Aggregation.createFromStr(XML.getValue(this.source, "aggregation"));
+    }
 
-    private Association.AssociationNode parseAssociationNodeElement(Element element) {
-        String name = element.getAttribute("sourceEndRoleName");
+    @Override
+    protected boolean isNavigableOwned() {
+        return XML.getBooleanValue(this.source, "isNavigableOwned");
+    }
+
+    @Override
+    protected Multiplicity parseMultiplicity() {
+        //return XML.getValue(this.source, )
         return null;
     }
 
-    @Override
-    public String parseName() {
+    private AssociationNode parseAssociationNode(Node node){
+        //return new AssociationNode();
         return null;
     }
 }
+
+
+
+
+
+//XmlNode sourceNode = aNode.SelectSingleNode("cd:relationshipOwnedElementsInternal",nsmgr).ChildNodes.Item(0).FirstChild;
+//XmlNode targetNode = aNode.SelectSingleNode("cd:relationshipOwnedElementsInternal", nsmgr).ChildNodes.Item(1).FirstChild;
+
+//a.Source = getCommonAssociationMemberAttributes(sourceNode);
+//a.Source.ClassName = className;
+//a.Source.ClassID = id;
+
+//a.Target = getCommonAssociationMemberAttributes(targetNode);
+//a.Target.ClassName = getAttribute(aNode.SelectSingleNode("cd:classMoniker",nsmgr),"LastKnownName");
+//a.Target.ClassID = getAttribute(aNode.SelectSingleNode("cd:classMoniker",nsmgr),"Id");
--- src/parser/classdiagram/xml/visualstudio15/VS15XMLClassParser.java
+++ src/parser/classdiagram/xml/visualstudio15/VS15XMLClassParser.java
@@ -5,6 +5,7 @@ import diagram.umlclass.Class;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import parser.XML;
 import parser.classdiagram.ClassParser;
 
 import javax.xml.xpath.XPath;
@@ -15,59 +16,43 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+
 public class VS15XMLClassParser extends ClassParser {
     private final Element classElement;
 
-    public Class getC() {
-        return c;
-    }
-
+//region Completed Implementation
     public VS15XMLClassParser(Element classElement) {
         this.classElement = classElement;
     }
 
     @Override
     public String parseName() {
-        return this.getValue(this.classElement, "name");
+        return XML.getValue(this.classElement, "name");
     }
 
     @Override
     public Visibility parseVisibility() {
-        String strVisibility = this.getValue(this.classElement, "visibility");
+        String strVisibility = XML.getValue(this.classElement, "visibility");
         return Visibility.createFromStr(strVisibility);
         //TODO: Should be exception handling here?
     }
 
     @Override
     public Boolean parseIsAbstract() {
-        return Boolean.parseBoolean(this.getValue(classElement, "isAbstract"));
+        return Boolean.parseBoolean(XML.getValue(classElement, "isAbstract"));
     }
+    //endregion
 
     @Override
     public Collection<Association> getAssociations() {
         List<Association> associations = new ArrayList<>();
-        NodeList associationNodes = this.getAssociationsNodes();
+        NodeList associationNodes = XML.getNodeList(this.classElement,"targetEnds/association");
         VS15XMLClassAssociationParser associationParser;
         for (int i = 0; i < associationNodes.getLength(); i++){
-            Element associationElement = (Element) associationNodes.item(i);
-            associationParser = new VS15XMLClassAssociationParser(associationElement);
-            associationParser.parse();
-            associations.add(associationParser.getAssociation());
+            associationParser = new VS15XMLClassAssociationParser(associationNodes.item(i));
+            associations.add(associationParser.parse());
         }
         return associations;
-    }
-
-    private NodeList getAssociationsNodes(){
-        //TODO: Handling when there is no association
-        XPath xPath = XPathFactory.newInstance().newXPath();
-        NodeList associationNodes = null;
-        try {
-            associationNodes = (NodeList)xPath.evaluate("targetEnds/association",
-            (Node)this.classElement, XPathConstants.NODESET);
-        } catch (XPathExpressionException e) {
-            e.printStackTrace();
-        }
-        return associationNodes;
     }
 
     @Override
@@ -85,10 +70,6 @@ public class VS15XMLClassParser extends ClassParser {
         return null;
     }
 
-    protected String getValue(Node node, String name){
-        Element e = (Element) node;
-        String value = e.getAttribute(name);
-        return value;
-    }
+
 
 }
