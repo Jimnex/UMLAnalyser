482914306a91c46df702025d22d55a8542262450
Izsóf Balázs <balazs.izsof@gmail.com>
2019-09-23 20:37:11 +0200

    Visual Studio 15 parser main fields parsed

--- src/parser/classdiagram/xml/visualstudio15/VS15XMLClassFieldParser.java
+++ src/parser/classdiagram/xml/visualstudio15/VS15XMLClassFieldParser.java
@@ -1,5 +1,56 @@
 package parser.classdiagram.xml.visualstudio15;
 
-public class VS15XMLClassFieldParser {
+import diagram.umlclass.Aggregation;
+import diagram.umlclass.Type;
+import diagram.umlclass.Visibility;
+import org.w3c.dom.Node;
+import parser.XML;
+import parser.classdiagram.FieldParser;
+import org.apache.*;
 
+public class VS15XMLClassFieldParser extends FieldParser {
+    private Node node;
+
+    public VS15XMLClassFieldParser(Node fieldNode) {
+        this.node = fieldNode;
+    }
+
+    @Override
+    protected Type parseType() {
+        Node typeNode = XML.getNode(this.node, "type_NamedElement");
+        String name = XML.getValue(typeNode,"LastKnownName");
+
+
+        return new Type();
+    }
+
+    @Override
+    protected Visibility parseVisibility() {
+        return Visibility.createFromStr(XML.getValue(this.node, "visibility"));
+    }
+
+    @Override
+    protected Boolean parseIsStatic() {
+        return XML.getBooleanValue(this.node,"isStatic");
+    }
+
+    @Override
+    protected Boolean parseIsReadOnly() {
+        return XML.getBooleanValue(this.node,"isReadOnly");
+    }
+
+    @Override
+    protected Aggregation parseAggregation() {
+        return Aggregation.createFromStr(XML.getValue(this.node, "aggregation"));
+    }
+
+    @Override
+    protected Boolean parseIsComposite() {
+        return XML.getBooleanValue(this.node,"isComposite");
+    }
+
+    @Override
+    public String parseName() {
+        return XML.getValue(this.node,"name");
+    }
 }
