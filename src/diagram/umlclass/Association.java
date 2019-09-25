bddc83e4aa960d7a8fbb3ec81e62d003f1ccff29
Izsóf Balázs <balazs.izsof@gmail.com>
2019-09-25 21:43:06 +0200

    clarifying Association abstraction

--- src/diagram/umlclass/Association.java
+++ src/diagram/umlclass/Association.java
@@ -1,11 +1,38 @@
 package diagram.umlclass;
 
 public class Association {
-    AssociationNode source;
-    AssociationNode target;
+    Node source;
+    Node target;
 
-    public Association(AssociationNode source, AssociationNode target){
+    public Association(Node source, Node target){
         this.source = source;
         this.target = target;
+    }
+
+    public static class Node {
+        public Node(String name, Boolean isComposite, Aggregation aggregation, Boolean isNavigableOwned, Multiplicity lower, Multiplicity upper) {
+            this.name = name;
+            this.isComposite = isComposite;
+            this.aggregation = aggregation;
+            this.isNavigableOwned = isNavigableOwned;
+            this.lower = lower;
+            this.upper = upper;
+        }
+
+        public Node(String name) {
+            this.name = name;
+        }
+
+        private String name;
+        private Boolean isLeaf;
+        private Boolean isStatic;
+        private Boolean isReadOnly;
+        private Boolean isDerived;
+        private Boolean isDerivedUnion;
+        private Boolean isComposite;
+        private Aggregation aggregation;
+        private Boolean isNavigableOwned;
+        private Multiplicity lower;
+        private Multiplicity upper;
     }
 }
