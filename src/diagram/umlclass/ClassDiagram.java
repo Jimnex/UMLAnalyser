720ca650d8c3f267c28a0029cc356829ebd56a35
Izsóf Balázs <balazs.izsof@gmail.com>
2019-09-28 15:31:29 +0200

    id and class object AbstractMap implementation for O(1) search

--- src/diagram/umlclass/ClassDiagram.java
+++ src/diagram/umlclass/ClassDiagram.java
@@ -6,23 +6,23 @@ import diagram.Diagram;
 import parser.Parser;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class ClassDiagram extends Diagram {
-    private final List<Class> classes;
+    private final AbstractMap<String, Class> classes;
+    private final AbstractMap<String, Interface> interfaces;
 
-    public ClassDiagram(String name, List<Class> classes) {
+    public ClassDiagram(String name, AbstractMap<String, Class> classes, AbstractMap<String, Interface> interfaces) {
         super(name);
         this.classes = classes;
+        this.interfaces = interfaces;
     }
 
-    public void addClass(Class c){
-        classes.add(c);
+    public void addClass(String id, Class c){
+        classes.put(id, c);
     }
 
-    public void removeClass(Class c){
-        classes.remove(c);
+    public Class getClass(int index){
+        return classes.remove(index);
     }
 }
