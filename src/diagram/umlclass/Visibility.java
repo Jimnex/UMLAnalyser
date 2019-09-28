0584008c01553d94558aa74948f5755965c8e6c3
Izsóf Balázs <balazs.izsof@gmail.com>
2019-09-28 12:48:04 +0200

    PACKAGE Visibility init, and createFromStr change

--- src/diagram/umlclass/Visibility.java
+++ src/diagram/umlclass/Visibility.java
@@ -3,16 +3,20 @@ package diagram.umlclass;
 public enum Visibility {
     PUBLIC,
     PRIVATE,
-    INTERNAL;
+    INTERNAL,
+    PACKAGE;
 
     public static Visibility createFromStr(String str) throws EnumConstantNotPresentException{
-        if (str.equals("Public")){
+        if (str.equals("")){
             return Visibility.PUBLIC;
         } else if (str.equals("Private")){
             return Visibility.PRIVATE;
         } else if (str.equals("Internal")) {
             return Visibility.INTERNAL;
-        } else throw new EnumConstantNotPresentException(Visibility.class, str);
-    }
-
+        } else if (str.equals("Package")) {
+            return Visibility.PACKAGE;
+        }
+            throw new EnumConstantNotPresentException(Visibility.class, str);
+        }
 }
+
