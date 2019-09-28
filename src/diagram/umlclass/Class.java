854ab48aedb17824edfed034037c6ce147ba09a2
Izsóf Balázs <balazs.izsof@gmail.com>
2019-09-28 14:24:07 +0200

    clear Class structure

--- src/diagram/umlclass/Class.java
+++ src/diagram/umlclass/Class.java
@@ -1,116 +1,26 @@
 package diagram.umlclass;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 
 public class Class {
-    public Class(String name) {
-        this.name = name;
-    }
     private String name;
     private Boolean isAbstract;
     private Boolean isStatic;
     private String id;
-
-    public Visibility getVisibility() {
-        return visibility;
-    }
-
-    public void setVisibility(Visibility visibility) {
-        this.visibility = visibility;
-    }
-
     private Visibility visibility;
-    private Collection<Class> childs = new ArrayList<Class>();
-    private Collection<Class> bases = new ArrayList<Class>();
-    private Collection<String> baseIds = new ArrayList<String>();
+    private List<String> childIds = new ArrayList<>();
+    private List<String> baseIds = new ArrayList<>();
+    private List<Association> associations = new ArrayList<Association>();
+    private List<Field> fields = new ArrayList<Field>();
+    private List<Method> methods = new ArrayList<Method>();
+    private List<Dependency> dependencies = new ArrayList<Dependency>();
 
-    private Collection<Association> associations = new ArrayList<Association>();
-    private Collection<Field> fields = new ArrayList<Field>();
-    private Collection<Method> methods = new ArrayList<Method>();
-    private Collection<Dependency> dependencies = new ArrayList<Dependency>();
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public Class(String name) {
         this.name = name;
     }
 
-    public Boolean getAbstract() {
-        return isAbstract;
-    }
-
-    public void setAbstract(Boolean anAbstract) {
-        isAbstract = anAbstract;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Collection<Class> getChilds() {
-        return childs;
-    }
-
-    public void setChilds(Collection<Class> childs) {
-        this.childs = childs;
-    }
-
-    public Collection<Class> getBases() {
-        return bases;
-    }
-
-    public void setBases(Collection<Class> bases) {
-        this.bases = bases;
-    }
-
-    public Collection<String> getBaseIds() {
-        return baseIds;
-    }
-
-    public void setBaseIds(Collection<String> baseIds) {
-        this.baseIds = baseIds;
-    }
-
-    public Collection<Association> getAssociations() {
-        return associations;
-    }
-
-    public void setAssociations(Collection<Association> associations) {
-        this.associations = associations;
-    }
-
-    public Collection<Field> getFields() {
-        return fields;
-    }
-
-    public void setFields(Collection<Field> fields) {
-        this.fields = fields;
-    }
-
-    public Collection<Method> getMethods() {
-        return methods;
-    }
-
-    public void setMethods(Collection<Method> methods) {
-        this.methods = methods;
-    }
-
-    public Collection<Dependency> getDependencies() {
-        return dependencies;
-    }
-
-    public void setDependencies(Collection<Dependency> dependencies) {
-        this.dependencies = dependencies;
-    }
-
-    public Class(String name, Boolean isAbstract, Boolean isStatic, Visibility visibility, Collection<Association> associations, Collection<Field> fields, Collection<Method> methods) {
+    public Class(String name, Boolean isAbstract, Boolean isStatic, Visibility visibility, List<Association> associations, List<Field> fields, List<Method> methods) {
         this.name = name;
         this.isAbstract = isAbstract;
         this.isStatic = isStatic;
@@ -119,4 +29,45 @@ public class Class {
         this.fields = fields;
         this.methods = methods;
     }
+
+    public Visibility getVisibility() {
+        return visibility;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Boolean getAbstract() {
+        return isAbstract;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public List<String> getChilds() {
+        return new ArrayList<>(this.childIds);
+    }
+
+    public List<String> getBaseIds() {
+        return new ArrayList<>(this.baseIds);
+    }
+
+    public List<Association> getAssociations() {
+        return new ArrayList<>(this.associations);
+    }
+
+    public List<Field> getFields() {
+        return new ArrayList<>(this.fields);
+    }
+
+    public List<Method> getMethods() {
+        return new ArrayList<>(this.methods);
+    }
+
+    public List<Dependency> getDependencies() {
+        return dependencies;
+    }
+
 }
